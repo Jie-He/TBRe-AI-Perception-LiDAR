f6f1e218a5d9ad1860bfb22320cc5a666605b346
Jie He <Jiehe1998yd@gmail.com>
2020-04-04 19:29:26 +0100

    Fixed plane calculation

--- src/perception.cpp
+++ src/perception.cpp
@@ -28,13 +28,28 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-const int nGridX = 6; // Number of grids along x (left - right)
-const int nGridZ = 6; // Number of grids along z (front - back)
-const float fThresh = 0.05f; // in meter
-const int nImageSw =  30;
-const int nImageSh = 60;
- 
+// Classification
+#include <tiny_dnn/tiny_dnn.h>
+#include "cone.h"
+
+#include <fstream>
+
 using namespace cv;
+using namespace tiny_dnn;
+using namespace tiny_dnn::layers;
+using namespace tiny_dnn::activation;
+
+const int nGridX = 8; // Number of grids along x (left - right)
+const int nGridZ = 8; // Number of grids along z (front - back)
+const float fThresh = 0.05f; // in meter
+const int nImageSw =  32;
+const int nImageSh = 32;
+
+// For data collection
+int record_index = -1;
+
+// Create a network 
+network<sequential> net;
 
 inline void print_point(pcl::PointXYZRGB p){
   std::cout << " X: " << p.x
@@ -50,14 +65,15 @@ void view_point_cloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud){
 
 void downsampling(pcl::PointCloud<pcl::PointXYZRGB>::Ptr pcloud,
                   pcl::PointCloud<pcl::PointXYZRGB>::Ptr sample){
+  
     // Perform downsampling with voxel
-  float fleaf = 0.02f; // Determine the filter size
+  float fleaf = 0.03f; // Determine the filter size
   pcl::VoxelGrid<pcl::PointXYZRGB> vg;
   vg.setInputCloud(pcloud);
   vg.setLeafSize(fleaf, fleaf, fleaf);
   vg.filter(*sample);
-
-  std::cerr << "PointCloud after filtering: " << pcloud->points.size()
+  
+  std::cerr << "PointCloud after filtering: " << sample->points.size()
        << " data points \n";
 }
 
@@ -67,10 +83,11 @@ void ground_removal(pcl::PointCloud<pcl::PointXYZRGB>::Ptr pcloud,
   pcl::PointXYZRGB minPoint, maxPoint;
   pcl::getMinMax3D(*pcloud, minPoint, maxPoint);
 
+  /**
   std::cout << "min point: ";
   print_point(minPoint);
   std::cout << "max point: ";
-  print_point(maxPoint);
+  print_point(maxPoint); **/
 
   // Calc the size of a grid
   float fGridX = (maxPoint.x - minPoint.x) / nGridX;
@@ -80,8 +97,9 @@ void ground_removal(pcl::PointCloud<pcl::PointXYZRGB>::Ptr pcloud,
   fGridX = (fGridX > 0)? fGridX : (-1 * fGridX);
   fGridZ = (fGridZ > 0)? fGridZ : (-1 * fGridZ);
 
+  /**
   std::cout << "GridWidth: " << fGridX 
-            << " GridLength: " << fGridZ << std::endl;
+            << " GridLength: " << fGridZ << std::endl; **/
 
   pcl::PointXYZ minBox, maxBox; // the 3D point of a box
   // Assign the values for the first grid
@@ -112,21 +130,23 @@ void ground_removal(pcl::PointCloud<pcl::PointXYZRGB>::Ptr pcloud,
       grid.setMax(maxBox.getVector4fMap());
       grid.filter(*gCloud);
 
+      // move the grid point along z
+      minBox.z += fGridZ; 
+      maxBox.z += fGridZ;
+
       // Skip if no points
-      //if (gCloud->points.size() == 0){ continue; }
+      if (gCloud->points.size() == 0){ continue; }
 
       // Find minimum point
       pcl::getMinMax3D(*gCloud, localMin, localMax);
+      // SKip if this box is very flat
+      if (localMax.y - localMin.y < fThresh * 3.6) { continue;}
       // Keep everything between lower thresh hold and 5m from zero plane
       pass.setFilterLimits(localMin.y + fThresh, 1); 
       pass.filter(*gCloud);
 
       //view_point_cloud(gCloud);
       *pOut += *gCloud;
-
-      // move the grid point along z
-      minBox.z += fGridZ; 
-      maxBox.z += fGridZ;
     }
     // move the grid point along x
     minBox.x += fGridX;
@@ -139,6 +159,7 @@ void ground_removal(pcl::PointCloud<pcl::PointXYZRGB>::Ptr pcloud,
 }
 
 void euclidean_cluster(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud,
+                       pcl::PointCloud<pcl::PointXYZRGB>::Ptr original,
                       std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr>& collection){
   pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGB>);
   tree->setInputCloud(cloud);
@@ -147,11 +168,16 @@ void euclidean_cluster(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud,
   std::vector<pcl::PointIndices> cluster_indices;
   pcl::EuclideanClusterExtraction<pcl::PointXYZRGB> ec;
   ec.setClusterTolerance(0.1); // with in 10 cm?
-  ec.setMinClusterSize(5);
-  ec.setMaxClusterSize(200);
+  ec.setMinClusterSize(15);
+  ec.setMaxClusterSize(1000);
   ec.setSearchMethod(tree);
   ec.setInputCloud(cloud);
   ec.extract(cluster_indices);
+
+  // create filter object
+  pcl::CropBox<pcl::PointXYZRGB> grid;
+  grid.setInputCloud(original);
+  pcl::PointXYZRGB minBox, maxBox;
 
   // Make point cloud for each cluster
   for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin();
@@ -166,30 +192,70 @@ void euclidean_cluster(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud,
     cloud_cluster->height= 1;
     cloud_cluster->is_dense = true;
 
+    // Cone retrival from original
+    // With the bounding box on the downsampled point cloud
+    pcl::getMinMax3D(*cloud_cluster, minBox, maxBox);
+    grid.setMin(minBox.getVector4fMap());
+    grid.setMax(maxBox.getVector4fMap());
+    grid.filter(*cloud_cluster);
+
     collection.push_back(cloud_cluster);
   }
 }
 
 float vectorAngle(float x, float y) {
-    if (x == 0) // special cases
-        return (y > 0)? M_PI
-#include <opencv2/core.hpp>
-            : (y == 0)? 0
-            : M_PI * 3 / 4;
-    else if (y == 0) // special cases
-        return (x >= 0)? 0
-            : M_PI;
-    float ret = atanf((float)y/x);
-    if (x < 0 && y < 0) // quadrant Ⅲ
-        ret = M_PI + ret;
-    else if (x < 0) // quadrant Ⅱ
-        ret = M_PI + ret; // it actually substracts
-    else if (y < 0) // quadrant Ⅳ
-        ret = (M_PI * 3 / 4) + (M_PI_2 + ret); // it actually substracts
-    return ret;
+  // add minimal noise to eliminate special cases
+  // Would be fine, given that there is a minimum range on LiDAR
+  if (x == 0) x = 0.001;
+  if (y == 0) y = 0.001;
+
+  float ret = atanf(fabs((float)y/x));
+  if ( x > 0 && y < 0) return (M_PI - ret);
+  if ( x < 0 && y > 0) return -ret;
+  if ( x < 0 && y < 0) return (ret - M_PI);
+
+  return atanf((float)y/x);
 }
 
-void project_to_image(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud){
+// This function will record the data
+void record_image(cv::Mat& image){
+  cv::namedWindow( "Sample", cv::WINDOW_AUTOSIZE );
+  cv::imshow( "Sample", image);
+
+  cv::namedWindow( "Gray", cv::WINDOW_AUTOSIZE );
+  cv::Mat g;
+  cv::cvtColor(image, g, cv::COLOR_RGB2GRAY);
+  cv::imshow("Gray", g);
+
+  waitKey(100);
+
+  if (record_index == -1){// ask for starting index to record 
+    std::cin.clear();
+    std::cout << "Enter the starting index: ";
+    std::cin >> record_index;
+  }
+
+  char ctype = 0;
+  std::cin.clear();
+  std::cin.ignore();
+  std::cout << "Class of this cone: ";
+  std::cin >> ctype;
+
+  if (ctype != '9'){
+    // save the image and append the label;
+    // Using default locations. (Ain't no time to make it fancy)
+    imwrite("res/images/img" + std::to_string(record_index) + ".png", image);
+    std::ofstream outfile;
+    outfile.open("res/labels.txt", std::ios_base::app);
+    outfile << ctype << "\n";
+    outfile.close();
+
+    record_index++;
+    std::cout << "Saved! " << record_index << std::endl;
+  }
+}
+
+void project_to_image(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud, cv::Mat& image){
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr projection (new pcl::PointCloud<pcl::PointXYZRGB>);
 
   // Calculate the plane perpendicualr to the centre of bounding box
@@ -200,15 +266,15 @@ void project_to_image(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud){
   centrePoint.x = ( minBox.x + maxBox.x ) / 2;
   centrePoint.z = ( minBox.z + maxBox.z ) / 2;
   // Calc rad
-  float direction = vectorAngle(centrePoint.x, centrePoint.y);
+  float direction = vectorAngle(centrePoint.x, centrePoint.z);
 
   // Create a set of planar coeff 
   pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients ());
   coefficients->values.resize (4);
-  coefficients->values[0] = sin(direction);
-  coefficients->values[1] = 0.0;
-  coefficients->values[2] = cos(direction);
-  coefficients->values[3] = 0.0;
+  coefficients->values[0] = cos(direction); // x
+  coefficients->values[1] = 0.0;            // y
+  coefficients->values[2] = sin(direction); // z
+  coefficients->values[3] = 0;
 
   // Create the filtering object
   pcl::ProjectInliers<pcl::PointXYZRGB> proj;
@@ -216,93 +282,166 @@ void project_to_image(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud){
   proj.setInputCloud(cloud);
   proj.setModelCoefficients(coefficients);
   proj.filter(*projection);
+  
+  // Rotate the plane such that its parallel to the XY plane
+  float rotation = direction + M_PI/2; // Some how it just works with this value.
+
+  Eigen::Affine3f transform_plane = Eigen::Affine3f::Identity();
+  transform_plane.rotate(Eigen::AngleAxisf (rotation, Eigen::Vector3f::UnitY()));
+  // Execute rotation
+  pcl::transformPointCloud(*projection, *projection, transform_plane);
 
   // Calculate the image bound on this flat cloud
   pcl::getMinMax3D(*projection, minBox, maxBox);
+
+  std::cout << "Projection size: " << projection->size() << std::endl;
+
   // Create black image of size nImageS * nImageS
-  cv::Mat boundImage(nImageSh, nImageSw, CV_8UC3, cv::Scalar(211, 0, 148)); 
-  std::cout << "width: " << maxBox.x - minBox.x << " height: " << maxBox.y - minBox.y << std::endl;
-  print_point(maxBox);
-  print_point(minBox);
+  image = cv::Mat(nImageSh, nImageSw, CV_8UC3, cv::Scalar(12,12,12)); 
+
   float ratio_x = nImageSw / (maxBox.x - minBox.x);
   float ratio_y = nImageSh / (maxBox.y - minBox.y); 
 
-  std::cout << "Was here\n";
-
-  for (std::size_t i = 0; i < cloud->points.size (); ++i){
-    //std::cout << "x: " << fabs(cloud->points[i].x - minBox.x) * ratio_x << " "
-    //          << "y: " << fabs(cloud->points[i].y - maxBox.y) * ratio_y << std::endl;
-    cv::circle(boundImage, Point(fabs(cloud->points[i].x - minBox.x) * ratio_x,
-                                 fabs(cloud->points[i].y - maxBox.y) * ratio_y), 2, 
-                              Scalar(cloud->points[i].b, cloud->points[i].g, cloud->points[i].r), 2);
+  for (std::size_t i = 0; i < projection->points.size (); ++i){
+    cv::circle(image, Point(fabs(projection->points[i].x - minBox.x) * ratio_x,
+                                 fabs(projection->points[i].y - maxBox.y) * ratio_y), 2, 
+                              Scalar(projection->points[i].b, projection->points[i].g, projection->points[i].r), 2);
   }
-  
-  cv::namedWindow( "Sample", cv::WINDOW_AUTOSIZE );
-  cv::imshow( "Sample", boundImage);
 
-  waitKey(0); 
-
-  //view_point_cloud(projection);
+  //record_image(image);
+  // /view_point_cloud(projection);
 }
 
-int main(int argc, char** argv){
+void get_cone_list(std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr>& cluster_collection,
+                   std::vector<cone>& cone_collection){
+  cv::Mat image; // Cast each cluster to a plane and store as image
+  cv::Mat_<uint8_t> gray_scale;
+  pcl::PointXYZRGB minp, maxp; // for the cluster's centroid
+  cone thisCone;
+  for (size_t i = 0; i < cluster_collection.size(); ++i){
+    project_to_image(cluster_collection[i], image); 
+    pcl::getMinMax3D(*cluster_collection[i], minp, maxp);
+    // Convert the colour image to gray scale
+    cv::cvtColor(image, gray_scale, cv::COLOR_RGB2GRAY);
+    vec_t  vimage;
+    std::transform(gray_scale.begin(), gray_scale.end(), std::back_inserter(vimage),
+                  [=](uint8_t c) {return c; });
+
+    thisCone.cone_class = net.predict_label(vimage);
+    thisCone.cone_accuy = (int)(net.predict(vimage)[thisCone.cone_class]);
+
+    // if the accuracy of the prediction is less than 70 then declare as unknown.
+    //if (thisCone.cone_accuy < 70) thisCone.cone_class = -1 ;
+
+    thisCone.x = (maxp.x + minp.x) / 2; // Horizontal 
+    thisCone.y = (maxp.y + minp.y) / 2; // Vertical
+    thisCone.z = (maxp.z + maxp.z) / 2; // Sideways 
+    cone_collection.push_back(thisCone);
+  }
+
+}
+
+void initialise(const std::string& weight){
+  // Load the network
+  // Define the layers
+  net << conv(32, 32, 5, 1, 6, padding::same) << activation::tanh()   //in 32x32x1, 5x5conv 6fmap
+      << max_pool(32, 32, 6, 2) << activation::tanh()                 //in 32x32x6, 2x2pooling
+      << conv(16, 16, 5, 6, 16, padding::same) << activation::tanh()  //in 16x16x6, 5x5conv 16fmaps
+      << max_pool(16, 16, 16, 2) << activation::tanh()                //in 16x16x16, 2x2pooling
+      << fc(8*8*16, 128) << activation::tanh()
+      << fc(128, 64) << activation::tanh()
+      << fc(64, 16) << activation::tanh()
+      << fc(16, 4) << softmax();                                    //in 16 out 4
+
+  net.load(weight);
+}
+
+void detection(pcl::PointCloud<pcl::PointXYZRGB>::Ptr pcloud, 
+               std::vector<cone>& cone_collection){
   // Start time
   std::chrono::steady_clock::time_point begin =\
   std::chrono::steady_clock::now();
 
-  // The original input/file
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr pcloud (new pcl::PointCloud<pcl::PointXYZRGB>);
   // Downsampled point cloud
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr sample (new pcl::PointCloud<pcl::PointXYZRGB>);
   // Ground removed & clustering
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr kcloud (new pcl::PointCloud<pcl::PointXYZRGB>);
   // Cluster List
-  std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> clust_collection;
+  std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> cluster_collection;
 
+  // Downsampling
+  downsampling(pcloud, sample);
+
+  // Ground removal
+  ground_removal(sample,  kcloud);
+
+  // Euclidean Clustering
+  // Clusters will be append into cluster_colletion
+  euclidean_cluster(kcloud, pcloud, cluster_collection);
+
+  // Classify each cluster
+  get_cone_list(cluster_collection, cone_collection);
+  
+  std::chrono::steady_clock::time_point ending =\
+  std::chrono::steady_clock::now();
+
+  std::cout << "DETECTION TIME = " 
+            << std::chrono::duration_cast<std::chrono::milliseconds>(ending - begin).count()
+            << "[ms]" << std::endl;
+
+  //std::cout << "Cone Collecction Size: "<< cone_collection.size() << std::endl;
+  std::cout << "Number of clusters:: " << cluster_collection.size() << std::endl;
+
+  // View result
+  //view_point_cloud(pcloud);
+  //view_point_cloud(kcloud);
+}
+
+int main(int argc, char** argv){
+
+  // The original input/file
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr pcloud (new pcl::PointCloud<pcl::PointXYZRGB>);
+  
   // Read the input pcd file
+  std::chrono::steady_clock::time_point begin =\
+  std::chrono::steady_clock::now();  
   if (argc > 1){
     if (pcl::io::loadPCDFile<pcl::PointXYZRGB> (argv[1], *pcloud) == -1){ 
       // load the file
       PCL_ERROR( "Couldn't read file. \n");
-      return (-1);
     }
   }else{
     std::cout << "No input .pcd file!" << std::endl;
     return (0);
-  }
+  }  
+  
+  std::chrono::steady_clock::time_point ending =\
+  std::chrono::steady_clock::now();
+  
+  std::cout << "PCD LOADING TIME = " 
+            << std::chrono::duration_cast<std::chrono::milliseconds>(ending - begin).count()
+            << "[ms]" << std::endl;
 
   std::cout << "Loaded "
             << pcloud->width * pcloud ->height
             << " data points from the test.pcd with the following fields: "
             << std::endl;
 
-  // Downsampling
-  downsampling(pcloud, sample);
-
-  // Ground removal
-  ground_removal(sample, kcloud);
-
-  // Euclidean Clustering
-  // Clusters will be append into cluster_colletion
-  euclidean_cluster(kcloud, clust_collection);
-
-  // Project each cluster to an image
-  for (size_t i = 0; i < clust_collection.size(); ++i)
-    project_to_image(clust_collection[i]);
-
-
-  std::chrono::steady_clock::time_point ending =\
-  std::chrono::steady_clock::now();
-
-  std::cout << "Time difference = " 
-            << std::chrono::duration_cast<std::chrono::milliseconds>(ending - begin).count()
-            << "[ms]" << std::endl;
-
-  std::cout << "Number of clusters:: " << clust_collection.size() << std::endl;
-
-  // View result
-  view_point_cloud(pcloud);
-  view_point_cloud(kcloud);
+  // initialise the network 
+  initialise("res/AMZ_VODKA");
+  
+  // List of result
+  std::vector<cone> cone_collection;
+  // Run detector
+  detection(pcloud, cone_collection);
+  // print list of detection
+  for (size_t i = 0; i < cone_collection.size(); i++){
+    std::cout << "Class: " << cone_collection[i].cone_class
+              << " X: " << cone_collection[i].x
+              << " Y: " << cone_collection[i].y
+              << " Z: " << cone_collection[i].z
+              << std::endl;
+  }
 
   return (0);  
 }
